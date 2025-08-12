8d9a11eb01ae5c0c5a3c54f10479006a35e2f475
Anastasya <aamokeeva@yandex.ru>
2025-08-12 17:35:29 +0300

    Добавляет кастомную директиву

--- src/components/CitySelect.vue
+++ src/components/CitySelect.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, onWatcherCleanup, ref, watch, watchEffect } from 'vue';
+import { onMounted, ref } from 'vue';
 import Button from './Button.vue';
 import IconLocation from './icons/IconLocation.vue';
 import Input from './Input.vue';
@@ -13,40 +13,9 @@ const emit = defineEmits({
 
 let city = ref('Vladimir');
 
-// onUpdated срабатывает при любом изменении компонента
-// чтобы следить за изменением конкретного реактивного свойства, используем watch
-// свойство city будет отслеживаться, и при его изменении будет вызываться колбек => оно передается первым аргументом
-// колбек передается вторым аргументом, и в нем будет функция, которая исполняется при изменении первого аргумента
-// третий аргумент - опции, здесь мы указываем immediate: true, чтобы колбек сработал сразу при создании компонента
-// похоже на computed, но здесь мы не создаем новое реактивное свойство, а просто следим за изменением существующего, и делаем что-то при этом
-watch(city, (newValue, oldValue) => {
-    console.log('City changed from', oldValue, 'to', newValue);
-
-    // очистка предыдущего состояния city, когда city изменится
-    onWatcherCleanup(() => {
-        console.log('Cleanup for city watcher');
-    });
-}, {
-    immediate: true, // срабатывает сразу при создании компонента, независимо от изменений city
-    once: true // срабатывает только один раз, а потом не будет отслеживать все изменения. Используется не так часто
-});
-
 // состояние определяет, находимся ли мы в режиме редактирования
 // по умолчанию - false, мы не редактируем город
 const isEdited = ref(false);
-
-// watchEffect позволяет отслеживать не одно свойство, а сразу несколько реактивных переменных
-// принимает сразу колбек функцию, не принимает аргументы с названиями переменных
-// любая реактивная переменная, которая используется внутри этой функции, будет отслеживаться
-// часто используется при получении данных с сервера, когда нужно следить за несколькими переменными
-// если в дальнейшем нужно остановить отслеживание, то нужно сохранить результат вызова watchEffect в переменную, например, unwatch, и вызвать ее
-const unwatch = watchEffect(() => {
-    console.log('Watch effect Current city:', city.value);
-    console.log('Watch effect Is edited:', isEdited.value);
-});
-
-// вызываем переменную, в которой сохранен результат вызова watchEffect, чтобы остановить отслеживание
-unwatch(); // остановка отслеживания, если нужно
 
 onMounted(() => {
     console.log('CitySelect component mounted');
@@ -66,8 +35,9 @@ function edit() {
 
 <template>
     <div class="city-select">
-        <div v-show="isEdited" class="city-input">
+        <div v-if="isEdited" class="city-input">
             <Input 
+                v-focus 
                 placeholder="Введите город" 
                 v-model="city" 
                 @keyup.enter="selectCity()" 
@@ -77,7 +47,7 @@ function edit() {
             </Button>
         </div>
 
-        <Button v-show="!isEdited" @click="edit()">
+        <Button v-else @click="edit()">
             <IconLocation />
             Изменить город
         </Button>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// регистрируем кастомную директиву focus глобально
+// теперь она доступна во всех компонентах приложения
+// она будет автоматически применяться к элементам, когда они будут смонтированы
+app.directive('focus', {
+    // кастомная директива, завязана на жизненный цикл элемента
+    // будет применяться когда элемент появится в DOM
+    mounted: (el) => el.focus()
+});
+
+app.mount('#app')
